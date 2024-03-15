ab1c8363710edad268c35220bcd64a848f732c3b
Rupraj Singh <ruprajsingh1@gmail.com>
2024-03-15 18:58:09 +0530

    fixes

--- src/pages/quiz/data.js
+++ src/pages/quiz/data.js
@@ -1,7 +1,7 @@
 export const quizData = [
   {
     id: 1,
-    question: "Which architectural masterpiece is depicted in the image below?",
+    question: "Which architectural masterpiece is depicted in the image?",
     options: [
       "Eiffel Tower",
       "Burj Khalifa",
@@ -13,14 +13,14 @@ export const quizData = [
   },
   {
     id: 2,
-    question: "Identify the species of bird shown in the image below.",
+    question: "Identify the species of bird shown in the image.",
     options: ["Peacock", "Cockatoo", "Macaw", "Toucan"],
     correctOption: "Peacock",
     questionImage: require("../../assets/peacock.webp"),
   },
   {
     id: 3,
-    question: "Which cityscape is shown in the image below?",
+    question: "Which cityscape is shown in the image?",
     options: ["New York City", "Tokyo", "London", "Sydney"],
     correctOption: "Tokyo",
     questionImage: require("../../assets/tokyo.jpg"),
@@ -28,7 +28,7 @@ export const quizData = [
 
   {
     id: 4,
-    question: "Identify the hardware component shown in the image below:",
+    question: "Identify the hardware component shown in the image:",
     options: ["CPU", "GPU", "RAM", "SSD"],
     correctOption: "SSD",
     questionImage: require("../../assets/ssd.jpeg"),
@@ -36,7 +36,7 @@ export const quizData = [
   {
     id: 5,
     question:
-      "What is the name of the gaming console depicted in the image below?",
+      "What is the name of the gaming console depicted in the image?",
     options: [
       "PlayStation 5",
       "Xbox Series X",
@@ -49,14 +49,14 @@ export const quizData = [
   {
     id: 6,
     question:
-      "Identify the software development tool shown in the image below:",
+      "Identify the software development tool shown in the image:",
     options: ["Git", "Jenkins", "Docker", "Ansible"],
     correctOption: "Docker",
     questionImage: require("../../assets/docker1.png"),
   },
   {
     id: 7,
-    question: "Identify the brand of the VR headset shown in the image below:",
+    question: "Identify the brand of the VR headset shown in the image:",
     options: ["Oculus", "HTC Vive", "PlayStation VR", "Valve Index"],
     correctOption: "Oculus",
     questionImage: require("../../assets/oculus.webp"),
